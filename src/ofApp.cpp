de158bbf2b727683980411f10de42fa0ab19c167
MathiasDC <mathias.decoster@gmail.com>
2016-05-23 11:37:54 +0200

    more cleanup
    meer comments weg

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -6,13 +6,6 @@ using namespace cv;
 float dyingTime;
 
 
-//deel code kris Meeusen
-int nrOfCircles = 0;
-float rotation = 0;
-float rotationSteps = 0;
-float lastSwitch;
-//bool closing;
-//bool opening;
 
 void Glow::setup(const cv::Rect& track) {
 	color.setHsb(ofRandom(255), 250, 250);
@@ -38,7 +31,7 @@ void Glow::kill() {
 }
 
 void Glow::draw() {
-	//color.setHsb(static_cast<int>(ofGetElapsedTimef()) % 250, 250, 250);
+	
 	ofPushStyle();
 	float size = 1;
 	ofSetColor(255);
@@ -48,32 +41,20 @@ void Glow::draw() {
 		size = ofMap(ofGetElapsedTimef() - startedDying, 0, dyingTime, size, 0, true);
 	}
 	ofNoFill();
-	//ofDrawCircle(cur, size);
 	ofSetColor(color);
 	myPolylineDraw(all);
-	//all.
 	ofSetColor(255);
-	//ofDrawBitmapString(ofToString(label), cur);
 	ofPopStyle();
 }
 
 
 void ofApp::setup() {
-	createNewArcs();
-	//closing = false;
-	//opening = true;
 	ofSetVerticalSync(true);
 	ofBackground(0);
 
-	lastSwitch = ofGetElapsedTimef();
-	rotationSteps = 8;
-	snapCounter = 0;
-	bSnapshot = false;
 	contour = false;
 	arcScale = 0.30;
 
-	/*movie.load("video.mov");
-	movie.play();*/
 
 	ofSetBackgroundAuto(false);
 
@@ -83,25 +64,15 @@ void ofApp::setup() {
 	vidGrabber.setVerbose(true);
 	vidGrabber.setup(1920,1080);
 
-
-
 	colorImg.allocate(1920,1080);
 	grayImage.allocate(1920,1080);
 	grayBg.allocate(1920,1080);
 	grayDiff.allocate(1920,1080);
-	//beeldOpslaan.allocate(1920, 1080, OF_IMAGE_COLOR);
-	//standaard.allocate(1920, 1080, OF_IMAGE_COLOR);
 
-	//standaard.load("standaard.jpg");
-	//grayBg.setFromPixels(standaard.getPixels());
-
-	bLearnBakground = false;
 	bDebug = false;
 	backgroundAuto = false;
 
 	scale = 3;
-
-	ofSetWindowShape(vidGrabber.getWidth(), vidGrabber.getHeight());
 
 	
 	gui.setup(); // most of the time you don't need a name
@@ -134,11 +105,11 @@ void ofApp::setup() {
 	backgrounds.push_back(title5);
 	backgrounds.push_back(title6);
 	backgrounds.push_back(title7);
+
+	ofSetFullscreen(true);
 }
 
 void ofApp::update() {
-
-	rotation += rotationSteps;
 
 
 	vidGrabber.update();
@@ -176,20 +147,6 @@ void ofApp::draw() {
 		followers[i].draw();
 	}
 	backgrounds[backgroundNumber].draw(0,0, 1920,1080);
-	if (tron) {
-		for (int i = 0; i < contourFinder.size(); i++) {
-			ofPushMatrix();
-			glTranslatef(contourFinder.getCentroid(i).x, contourFinder.getCentroid(i).y, 0);
-			ofScale(arcScale, arcScale, 1);
-			for (int i = 0; i < nrOfCircles; i++)
-			{
-				arc* arcObject = arcs.at(i);
-				arcObject->draw(rotation);
-			}
-
-			ofPopMatrix();
-		}
-	}
 	if (bDebug) {
 		ofSetHexColor(0xffffff);
 		grayImage.draw(360, 20, 320, 180);
@@ -209,85 +166,7 @@ void ofApp::draw() {
 
 }
 
-void ofApp::createNewArcs()
-{
-	for (int i = 0; i < arcs.size(); i++) {
-		// not sure if cleans out everything...
-		delete arcs[i];
-	}
-	arcs.clear();
 
-	nrOfCircles = 15;
-
-	float innerRadius = 50;
-	float angle = 0;
-	float cirleAngle = 0;
-	bool clockWise = true;
-	float lineWidth = 10;
-	float margin = 0;
-	float circleStartAngle = 0;
-
-	for (int i = 0; i < nrOfCircles; i++) {
-
-		arc* arcObject = new arc();
-		arcObject->innerRadius = innerRadius;
-		arcObject->outerRadius = innerRadius + lineWidth;
-
-		angle += ofRandom(0, 10) > 2 ? 0.1 : 0;
-		arcObject->startAngle = angle;
-
-		angle += ofRandom(0.2, 4.5);
-		arcObject->endAngle = angle;
-
-		arcObject->clockwise = clockWise;
-
-		// using negative random values to avoid to much coloring
-		arcObject->colorBlueStartFactor = ofClamp(ofRandom(-0.4, 1.0), 0, 1);
-		arcObject->colorBlueEndFactor = ofClamp(ofRandom(-0.4, 1.0), 0, 1);
-		arcObject->colorGreenCorrection = ofClamp(ofRandom(-0.4, 0.1), 0, 1);
-
-
-		if (angle >= (TWO_PI + circleStartAngle)) {
-			innerRadius += (margin + lineWidth);
-
-			// do correction on last arc
-			arcObject->endAngle = TWO_PI + circleStartAngle;
-
-			angle = (angle - (TWO_PI));
-			circleStartAngle = angle;
-			clockWise = ofRandom(10) > 5;
-
-			int lineWidthRandom = ofRandom(20);
-			if (lineWidthRandom < 3) {
-				lineWidth = 30;
-			}
-			else if (lineWidth <10) {
-				lineWidth = 20;
-			}
-			else {
-				lineWidth = 10;
-			}
-
-
-			int marginRandom = ofRandom(0, 20);
-
-			if (marginRandom > 6) {
-				margin = 0;
-			}
-			else if (marginRandom > 2) {
-				margin = 10;
-			}
-			else
-			{
-				margin = 30;
-			}
-		}
-
-		arcObject->createMesh();
-		arcs.push_back(arcObject);
-
-	}
-}
 
 void ofApp::keyPressed(int key) {
 	switch (key) {
@@ -307,10 +186,6 @@ void ofApp::keyPressed(int key) {
 	case 'c':
 		//contour aanzetten
 		contour = !contour;
-		break;
-	case 't':
-		//troncirckelsaanzetten
-		tron = !tron;
 		break;
 	case '1':
 		backgroundNumber = 0;
